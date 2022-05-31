43e97d574fe32160444631e6608f633a53e1cc08
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-05-31 18:21:12 -0300

    add modulo

---
+++ modulos/jogo.py
@@ -0,0 +1,7 @@
+def canal():
+    print("CFB Cursos")
+
+jogador  ={"Nome": "Bruce",
+           "pontos": 100,
+           "energia": 75,
+           "time" : 1}
---
+++ modulos/main.py
@@ -0,0 +1,4 @@
+import jogo
+
+jogo.canal()
+print(jogo.jogador)
