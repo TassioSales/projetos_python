204408b38e1a4bbdd1af5f9748cf189b6e9db393
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-07-12 21:16:28 -0300

    atualizando codigo

--- banco_de_dados/agenda_sql/agenda.py
+++ banco_de_dados/agenda_sql/agenda.py
@@ -37,16 +37,20 @@ def consultar(conexao, sql):
 
 
 def menuPrincipal():
-    os.system("cls")
-    print("""
-    1 - Inserir novo contato
-    2 - Deletar Registro
-    3 - Alterar contato
-    4 - Consultar Registro (ID)
-    5 - Consulta contato (Nome)
-    6 - Sair""")
-    opcao = int(input("Digite a opção desejada: "))
-    return opcao
+    try:
+        os.system("cls")
+        print("""
+        1 - Inserir novo contato
+        2 - Deletar Registro
+        3 - Alterar contato
+        4 - Consultar Registros
+        5 - Consulta contato (Nome)
+        6 - Sair""")
+        opcao = int(input("Digite a opção desejada: "))
+        return opcao
+    except ValueError:
+        print("Opção inválida!")
+        return 0
 
 
 def inserirContato():
@@ -67,7 +71,7 @@ def deletarContato():
 
 def alterarContato():
     vid = input("Digite o ID do contato que deseja alterar: ")
-    resp =consultar(vcon, "SELECT * FROM tb_contatos WHERE ID = " + vid)
+    resp = consultar(vcon, "SELECT * FROM tb_contatos WHERE ID = " + vid)
     rnome = resp[0][1]
     rtelefone = resp[0][2]
     remail = resp[0][3]
@@ -84,44 +88,80 @@ def alterarContato():
     query(vcon, vsql)
 
 
-
-
 def consultarContato():
-    pass
+    os.system("cls")
+    cont = 0
+    vslq = "SELECT * FROM tb_contatos"
+    for i in consultar(vcon, vslq):
+        print(f'ID:  {i[0]:_<3}, Nome: , {i[1]:_<30}, Telefone: , {i[2]:_<14}, Email: , {i[3]:_<30}')
+        cont += 1
+        if cont == 10:
+            resp = input("Deseja continuar? (S/N) ").upper().strip()[0]
+            if resp == "S":
+                os.system("pause")
+                os.system("cls")
+                cont = 0
+            else:
+                break
+    print(f"Total de {cont} registros")
+    print("Fim da lista")
+    os.system("pause")
 
 
 def consultarContatoNome():
-    pass
+    os.system("cls")
+    cont = 0
+    vnome = input("Digite o nome do contato: ")
+    vsql = "SELECT * FROM tb_contatos WHERE NOME LIKE '%" + vnome + "%'"
+    resp = consultar(vcon, vsql)
+    for i in resp:
+        print(f'ID:  {i[0]:_<3}, Nome: , {i[1]:_<30}, Telefone: , {i[2]:_<14}, Email: , {i[3]:_<30}')
+        cont += 1
+        if cont == 10:
+            resp = input("Deseja continuar? (S/N) ").upper().strip()[0]
+            if resp == "S":
+                os.system("pause")
+                os.system("cls")
+                cont = 0
+            else:
+                break
+    print(f"Total de {cont} registros")
+    print("Fim da lista")
+    os.system("pause")
 
 
-while True:
-    opt = menuPrincipal()
-    if opt == 1:
-        # Inserir novo contato
-        inserirContato()
-    elif opt == 2:
-        # Deletar Registro
-        deletarContato()
-    elif opt == 3:
-        # Alterar contato
-        alterarContato()
-    elif opt == 4:
-        # Consultar Registro (ID)
-        consultarContato()
-    elif opt == 5:
-        # Consulta contato (Nome)
-        consultarContatoNome()
-    elif opt == 6:
-        os.system("cls")
-        # Sair
-        print("Saindo do programa...")
-        os.system("pause")
-        break
-    else:
-        os.system("cls")
-        print("Opção inválida!")
-        os.system("pause")
-        continue
+if __name__ == "__main__":
+    while True:
+        opt = menuPrincipal()
+        if opt == 1:
+            # Inserir novo contato
+            inserirContato()
+        elif opt == 2:
+            # Deletar Registro
+            deletarContato()
+        elif opt == 3:
+            # Alterar contato
+            alterarContato()
+        elif opt == 4:
+            # Consultar Registro (ID)
+            consultarContato()
+        elif opt == 5:
+            # Consulta contato (Nome)
+            consultarContatoNome()
+        elif opt == 6:
+            os.system("cls")
+            # Sair
+            print("Saindo do programa...")
+            os.system("pause")
+            break
+        elif opt == 0:
+            os.system("cls")
+            os.system("pause")
+        else:
+            os.system("cls")
+            print("Opção inválida!")
+            os.system("pause")
+            continue
 
-vcon.close()
-os.system("pause")
+    vcon.close()
+    os.system("pause")
